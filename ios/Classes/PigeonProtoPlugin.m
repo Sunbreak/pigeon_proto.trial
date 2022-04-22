f3b8bb11a8075dd433f2d8798bbe46ba30e0ccf3
Sunbreak <sunbreak.wang@gmail.com>
2022-04-22 19:22:27 +0800

    Initial commit
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

---
+++ ios/Classes/PigeonProtoPlugin.m
@@ -0,0 +1,20 @@
+#import "PigeonProtoPlugin.h"
+
+@implementation PigeonProtoPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"pigeon_proto"
+            binaryMessenger:[registrar messenger]];
+  PigeonProtoPlugin* instance = [[PigeonProtoPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
